88898d6835a606415a53f23373c49fcb0a653d56
gurshaan17 <gurshaansinghd@gmail.com>
2024-10-20 12:03:57 +0530

    db set up

---
+++ src/db.ts
@@ -0,0 +1,35 @@
+require("dotenv").config()
+import mongoose, { Document, Schema } from "mongoose"
+
+interface Data extends Document{
+    id: string,
+    shortUrl: string,
+    content: string
+}
+
+const DataSchema = new Schema({
+    id: {type: String, required: true},
+    shortUrl: {type: String, required: true},
+    content: {type: String, required: true}
+})
+
+export const dataModel = mongoose.model<Data>("data", DataSchema)
+
+export const connectDB = async () =>{
+    try{
+        const mongoUri = process.env.MONGO_URI
+        if (!mongoUri) {
+            throw new Error("MONGO_URI is not defined in env");
+        }
+
+        await mongoose.connect(mongoUri, {
+            dbName: "main",
+        });
+
+        console.log("DB connected");
+    }
+    catch(err){
+        console.error("Database connection error:", err);
+        process.exit(1);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import { connectDB } from "./db";
 
 const app = express()
 app.use(express.json())
 
 
 async function startServer() {
+    await connectDB()
     app.post('/new', )
 }
 
