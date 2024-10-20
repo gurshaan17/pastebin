0535708e3d989ae901d1b98c133bc57df80eb646
gurshaan17 <gurshaansinghd@gmail.com>
2024-10-20 16:34:23 +0530

    paste received

--- src/getPaste.ts
+++ src/getPaste.ts
@@ -1,6 +1,28 @@
 import { Request, Response } from "express";
+import { dataModel } from "./db";
 
 
 export async function getPaste(req: Request, res: Response){
-    
+    const { id } = req.params;
+
+    if(!id){
+        res.status(400).json({ message: "ID required" })
+    }
+
+    const data = await dataModel.findOne({
+        id: id
+    })
+
+    if(!data){
+        res.status(400).json({ message: "paste not found"})
+    }
+    else{
+        res.status(200).json({
+            message: "paste found",
+            data: {
+                id: id,
+                content: data?.content
+            }
+        })
+    }
 }
